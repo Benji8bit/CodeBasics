9138a13e1cce90101d2c969d59d10731c553fee3
Benji8bit <coin8bit@hotmail.com>
2023-01-23 23:24:50 +0300

    Initial commit

---
+++ Module_1/hello.go
@@ -0,0 +1,10 @@
+package main
+
+// fmt package import
+import "fmt"
+
+//main function definition
+func main() {
+	//print function call from fmt package
+	fmt.Print("Hello, World!") // no need in semicolon
+}
